5aab467366412793c5aa5763cd29073c8c85402b
theknifewind <theknifewind44@gmail.com>
2025-03-31 18:49:47 +0530

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -4,3 +4,6 @@ git commit -m "Initial commit"
 git branch -M main
 git remote add origin https://github.com/theknifewind/music_recommendation_system.git
 git push -u origin main
+git add requirements.txt
+git commit -m "Added requirements.txt"
+git push origin main
